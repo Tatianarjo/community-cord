12cf57f86534d33c166027216c5a114548bbd8a2
Tatiana Johnson <tatianarjo@gmail.com>
2026-08-29 15:42:28 -0500

    adds clerk route and centers signup card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const font = Open_Sans({
   variable: "--font-geist-sans",
@@ -15,11 +16,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
+    <ClerkProvider>
     <html
       lang="en"
       className={`${font.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
